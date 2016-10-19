92a7ea4dd8f3a6e6ef2003793f312dee62ff1289
wangjianghua <wjh@xkeshi.com>
2016-10-19 13:43:24 +0800

    #add：新增接口类

---
+++ SparrowData/Src/DInterface.h
@@ -0,0 +1,13 @@
+//
+//  DInterface.h
+//  SparrowData
+//
+//  Created by microsparrow on 16/10/18.
+//  Copyright © 2016年 microsparrow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DInterface : NSObject
+
+@end
---
+++ SparrowData/Src/DInterface.m
@@ -0,0 +1,13 @@
+//
+//  DInterface.m
+//  SparrowData
+//
+//  Created by microsparrow on 16/10/18.
+//  Copyright © 2016年 microsparrow. All rights reserved.
+//
+
+#import "DInterface.h"
+
+@implementation DInterface
+
+@end
